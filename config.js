f33b827ed3413ee9522a1e568213b38657239e4a
yfsite <63030086+yfsite@users.noreply.github.com>
2020-05-01 19:20:58 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-// 配置
+﻿// 配置
 window.Config = {
 
   // 站点名
-  SiteName: 'Uptime Status',
+  SiteName: '网站状态',
 
   // 站点链接
   SiteUrl: '/',
@@ -10,10 +10,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur913412-dadd81ac58c8d079165c2959',
   ],
 
   // 是否显示监测站点的链接
@@ -22,17 +19,13 @@ window.Config = {
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 60,
+  CountDays: 31,
 
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: 'Blog',
+      url: 'https://www.yfblog.ml/index'
     },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
-    }
   ]
 };
